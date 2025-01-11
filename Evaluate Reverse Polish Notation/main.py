4db9fd071c7a65bd0172ecb38b5a55b9e1b76d40
James Mata <jamesmata556@gmail.com>
2025-01-10 20:13:01 -0500

    another one down

---
+++ Evaluate Reverse Polish Notation/main.py
@@ -0,0 +1,25 @@
+from typing import List
+
+class Solution:
+    def evalRPN(self, tokens: List[str]) -> int:
+
+        operators = {"+", "-", "*", "/"}
+
+
+        operator = ""
+        stack = []
+        answer = 0
+        for token in tokens:
+
+            if token in operators:
+                num1 = stack.pop()
+                num2 = stack.pop()
+                stack.append(str(int(eval(num2 + token + num1))))
+            
+            else:
+                stack.append(token)
+    
+        answer = int(stack[0])
+        return answer
+            
+        
